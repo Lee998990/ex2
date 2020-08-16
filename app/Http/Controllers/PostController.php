fe4732f402943432e87f4373f72a296adc90e893
Lee Lungta <monlungme@gmail.com>
2020-08-16 17:10:06 +0700

    'update

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,15 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use App\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
-    //
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::where('user_id', Auth::user()->id)->paginate(5);
         $data = [
             'posts' => $posts
         ];
@@ -23,19 +24,44 @@ class PostController extends Controller
         return $category;
     }
 
-    public function creat()
+    public function create()
+    {
+        $categories = Category::all();
+        $data = [
+            'categories' => $categories
+        ];
+        return view('post.create', $data);
+    }
+
+    public function store(Request $request)
+    {
+        $detail = $request->input('detail');
+        $category_id = $request->input('category_id');
+
+        $post = new Post();
+        $post->detail = $detail;
+        $post->category_id = $category_id;
+        $post->user_id = auth()->user()->id;
+        $post->complete = 0;
+        $post->save();
+
+        return redirect('/post');
+    }
+
+    public function edit($id)
     {
     }
-    public function store()
+
+    public function update(Request $request)
     {
     }
-    public function edit()
+
+    public function delete($id)
     {
+        $post = Post::find($id);
+        $post->delete();
+        return redirect('/post');
     }
-    public function update()
-    {
-    }
-    public function delete()
-    {
-    }
+
 }
+
